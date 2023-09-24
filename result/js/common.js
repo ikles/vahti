4f29b9eec3e91353fd6a2f09ec6ec013978fdde8
Igor <membship@ya.ru>
2023-09-24 21:43:20 +0700

    commit

--- result/js/common.js
+++ result/js/common.js
@@ -98,7 +98,7 @@ jQuery(document).ready(function( $ ) {
 
 /************************************/
 
-/*$('.wrapper').prepend('<span class="eye-3"></span>');
+$('.wrapper').prepend('<span class="eye-3"></span>');
 let pg = parseInt(document.location.pathname.match(/\d+/))
 $('body').addClass('active').css('background-image', "url('../img/"+pg+".jpg')");
 $('body:not(.active)').css('background-image', "unset");
@@ -109,7 +109,7 @@ $('.eye-3').click(function (e) {
   let pg = parseInt(document.location.pathname.match(/\d+/));
   $('body.active').css('background-image', "url('../img/"+pg+".jpg')");
   $('body:not(.active)').css('background-image', "unset");
-});*/
+});
 
 /************************************/
 
